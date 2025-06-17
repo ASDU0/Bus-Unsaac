6d5b80612bbdff233c65b91540b360b3026380e5
JoseAnton10 <183485@unsaac.edu.pe>
2025-06-16 22:01:55 -0500

    Subiendo mi trabajo Dia 1

---
+++ Pepe/Dia 1/01_hello_wordl.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  //var myName='Jose';
+  //late final myName;
+  //String myName = 'Jose';
+  const myName = 'Jose';
+  
+  print('Hola $myName');
+  
+  print('Hola ${ myName.toUpperCase()}');
+  
+  print('Hola ${1+1}');
+
+
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
+
+}
