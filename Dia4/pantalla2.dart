14f1e6b4bc131097255c55409cfbe10a3ac6760e
Eduardo Torre Cano <211361@unsaac.edu.pe>
2025-06-20 21:08:56 -0500

    incomplete 1

--- Dia4/pantalla2.dart
+++ Dia4/pantalla2.dart
@@ -1,1 +1,24 @@
+// FALTA IMPLEMENTAR 
+      
+
+'package:flutter/material.dart';
+
+class Pantalla2 extends StatelessWidget {
+  const Pantalla2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pantalla 2')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/pantalla3');
+          },
+          child: const Text('Ir a Pantalla 3'),
+        ),
+      ),
+    );
+  }
+}
 
