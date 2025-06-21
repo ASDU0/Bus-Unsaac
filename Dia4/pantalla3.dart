05ea41dfbbe6fc3c4b8f58ca312e27a172f93199
Eduardo Torre Cano <211361@unsaac.edu.pe>
2025-06-20 21:09:28 -0500

    Incomplete 2

--- Dia4/pantalla3.dart
+++ Dia4/pantalla3.dart
@@ -1,1 +1,21 @@
+// falta implementar acciones
+import 'package:flutter/material.dart';
 
+class Pantalla3 extends StatelessWidget {
+  const Pantalla3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pantalla 3')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          },
+          child: const Text('Volver a Pantalla 1'),
+        ),
+      ),
+    );
+  }
+}
